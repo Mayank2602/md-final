d9b979b6dee7972903c243cfb9086b862f908be4
MAYANK NARANG <mayank260202@gmail.com>
2022-04-13 15:19:05 +0530

    'init'

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -24,15 +24,11 @@ function Header() {
       </div>
       <div className="flex flex-row ">
         <a href='https://placementor-2021.herokuapp.com/' target={'_blank'}>
-      <div className="flex items-center space-x-5 rounded-full bg-gray-200 py-2 px-5 m-2 cursor-pointer font-serif"> 
-        <h3 >Placementor</h3>
-      </div>
+        <button type="button" className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Placementor</button>
       </a>
       {/* </Link> */}
       <a href='https://www.iitism.ac.in/iitismnew/' target='_blank'>
-      <div className="flex items-center space-x-5 rounded-full bg-gray-200 py-2 px-5 m-2 cursor-pointer font-serif">
-        <h3>IIT-ISM</h3>
-      </div>
+      <button type="button" className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">IIT-ISM</button>
       </a>
       </div>
     </header>
